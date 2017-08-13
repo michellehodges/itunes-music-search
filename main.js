f54c20bee3d9b00c48d8e5568bd4ed34f9fc3c7a
michellehodges <mischamangan@gmail.com>
2017-08-13 16:06:49 -0400

    Edits to page styling

--- main.js
+++ main.js
@@ -10,17 +10,24 @@ function convertToJson(songs) {
 
 function returnSongs(songs) {
   let htmlInsertionAtContainer = '';
+  let htmlInsertionSearchResults = '';
   let songsContainer = document.querySelector(".results");
 
   for (j = 0; j < songs.results.length; j++ ) {
     let song = songs.results[j];
     htmlInsertionAtContainer += `
-    <h1>Search Results:</h1>
-    <div class="search-result">
-      <img src=${songs.results[j].artworkUrl60} class="search-image">
-      <span>${songs.results[j].trackName}</span>
-      <span>${songs.results[j].artistName}</span>
-      <button type="button" class="play-now"  onclick="playSong('${songs.results[j].trackName}', '${songs.results[j].artistName}','${songs.results[j].previewUrl}');"
+    <div class="search-result"
+      style="
+        height: 200px;
+        width: 200px;
+        text-align: center;
+        font-family: 'Arvo', serif;
+        font-size: 15px;
+    ">
+      <img src=${songs.results[j].artworkUrl100} class="search-image" style="padding: 10px;"><br/>
+      <span>${songs.results[j].trackName}</span><br/>
+      <span>${songs.results[j].artistName}</span><br/>
+      <button type="button" class="play-now" style="margin: 10px;" onclick="playSong('${songs.results[j].trackName}', '${songs.results[j].artistName}','${songs.results[j].previewUrl}');"
        value=${songs.results[j].previewUrl}>Play Now</button>
     </div>
       `;
@@ -33,7 +40,13 @@ function playSong(trackName,artistName,previewUrl) {
   let htmlInsertionAtPlayer = '';
   htmlInsertionAtPlayer += `
   <audio class="music-player" controls="controls" src="${previewUrl}" autoplay controls></audio>
-  <span>Now Playing: ${artistName} - ${trackName} </span>
+  <span
+    style="
+      font-family: 'Arvo', serif;
+      font-size: 15px;
+      padding-left: 70px;
+  ">
+    Now Playing: ${artistName} - ${trackName} </span>
   `
   songPlayer.innerHTML = htmlInsertionAtPlayer;
 }
